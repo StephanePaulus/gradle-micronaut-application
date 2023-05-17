4a85e4771a9a96173a5ee7b18a4f221112090fb0
Stephane Paulus <spaulus@alpian.com>
2023-05-17 18:20:53 +0200

    Added jib

--- src/test/java/example/micronaut/HelloControllerTest.java
+++ src/test/java/example/micronaut/HelloControllerTest.java
@@ -7,32 +7,31 @@ import io.micronaut.http.HttpRequest;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-import org.junit.jupiter.api.Test;
-
 import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
 
 @MicronautTest // <1>
 public class HelloControllerTest {
 
-    @Inject
-    @Client("/")  // <2>
-    HttpClient client;
+  @Inject
+  @Client("/") // <2>
+  HttpClient client;
 
-    @Test
-    public void testHello() {
-        HttpRequest<String> request = HttpRequest.GET("/hello");  // <3>
-        String body = client.toBlocking().retrieve(request);
+  @Test
+  public void testHello() {
+    HttpRequest<String> request = HttpRequest.GET("/hello"); // <3>
+    String body = client.toBlocking().retrieve(request);
 
-        assertNotNull(body);
-        assertEquals("Hello world", body);
-    }
+    assertNotNull(body);
+    assertEquals("Hello world", body);
+  }
 
-    @Test
-    public void testTest() {
-        HttpRequest<String> request = HttpRequest.GET("/test");  // <3>
-        String body = client.toBlocking().retrieve(request);
+  @Test
+  public void testTest() {
+    HttpRequest<String> request = HttpRequest.GET("/test"); // <3>
+    String body = client.toBlocking().retrieve(request);
 
-        assertNotNull(body);
-        assertEquals("Test", body);
-    }
+    assertNotNull(body);
+    assertEquals("Test", body);
+  }
 }
